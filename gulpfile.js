2941aeb157b1fcaacf944a620a2411dfb4b69637
Jameson Knoll <j.knoll621@gmail.com>
2016-02-22 13:59:28 +0800

    Improve SCSS build and add gulp tasks for concat vendor libraries

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,11 @@
 var gulp = require('gulp');
 var wiredep = require('wiredep').stream;
 var sass = require('gulp-sass');
-var connect = require('gulp-connect');
-
+//var connect = require('gulp-connect');
+var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
+var inject = require('gulp-inject');
+var lib = require('bower-files')();
 
 // This shouldn't be a gulp task, it should be a bower post-install hook
 gulp.task('bower', function() {
@@ -16,32 +19,51 @@ gulp.task('bower', function() {
 		.pipe(gulp.dest('./dist'));
 });
 
-gulp.task('webserver', function() {
-	connect.server({
-		livereload: true
+
+gulp.task('webserver', function(){
+	browserSync.init({
+		server: {
+			baseDir: './dist/'
+		}
 	});
+
+	gulp.watch('src/scss/**/*.scss', ['styles']);
+	gulp.watch('src/**/*.html').on('change', browserSync.reload);
 });
 
+gulp.task('build-css', function(){
+	gulp.src(lib.ext('css').files)
+		.pipe(concat('vendor.css'))
+		.pipe(gulp.dest('./dist/css/'));
+});
 
-gulp.task('html', function() {
-	gulp.src('./src/**/*.html')
-		.pipe(gulp.dest('./dist/'))
-		.pipe(connect.reload());
+gulp.task('build-js', function(){
+	gulp.src(lib.ext('js').files)
+		.pipe(concat('vendor.js'))
+		.pipe(gulp.dest('./dist/js/'));
 });
 
 gulp.task('styles', function() {
-	gulp.src('./src/sass/**/*.scss')
+	gulp.src('./src/scss/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./dist/css/'))
-		.pipe(connect.reload());
+		.pipe(browserSync.stream());
 });
 
+gulp.task('index', function(){
+	var target = gulp.src('./dist/index.html');
+	var sources = gulp.src(['./dist/**/*.css', './dist/**/*.js'], {read: false});
+
+	return target.pipe(inject(sources))
+		.pipe(gulp.dest('./dist/'));
+
+});
 
 gulp.task('watch', function() {
 	gulp.watch('./src/sass/**/*.scss', ['styles']);
-	gulp.watch('./src/**/*.html', ['html']);
+	//gulp.watch('./src/**/*.html', ['html']);
 });
 
-gulp.task('default', ['bower']);
+gulp.task('default', ['build-css', 'build-js', 'styles', 'index', 'webserver', 'watch']);
 
-gulp.task('serve', ['html', 'bower', 'styles', 'webserver', 'watch']);
+gulp.task('serve', ['bower', 'styles', 'webserver', 'watch']);
